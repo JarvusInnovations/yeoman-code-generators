91c3b70088c49b90741bec0752c42775de209abf
Jim Smiley <skiver.kelvin@gmail.com>
2018-01-19 11:35:18 -0500

    remove unneccesary code

--- generator-vagrant/generators/app/index.js
+++ generator-vagrant/generators/app/index.js
@@ -9,21 +9,9 @@ module.exports = class extends Generator {
 
   //Writing Logic here
   writing() {
-
-    //return {
-        // config = function() {
-        //     throw Error("getting trigged?");
-            this.fs.copyTpl(
-                this.templatePath('_Vagrantfile'),
-                this.destinationPath('Vagrantfile'), {
-                }
-            );
-        // }
-    //};
-    //Copy the configuration files
-  
-    //Copy application files
-  
-    //Install Dependencies
+    this.fs.copyTpl(
+        this.templatePath('_Vagrantfile'),
+        this.destinationPath('Vagrantfile')
+    );
   }
 };
